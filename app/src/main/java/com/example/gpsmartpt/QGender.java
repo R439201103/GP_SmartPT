512b88b7b629ea90ed5280339e1a763359725cfb
R439201103 <r7r3l2gh@gmail.com>
2021-10-23 06:06:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gpsmartpt/QGender.java
@@ -0,0 +1,14 @@
+package com.example.gpsmartpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QGender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q_gender);
+    }
+}
